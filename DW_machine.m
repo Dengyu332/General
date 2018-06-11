17243957cb80a68f815df75db60d6953e32a1742
Wang <DEW93@pitt.edu>
2018-06-11 14:28:43 -0400

    change of DW_machine

--- DW_machine.m
+++ DW_machine.m
@@ -1,6 +1,6 @@
 % select machine
 
-Choice=input('Please specify mac(1) or linux(2):');
+Choice=input('Please specify mac(1), linux(2) or Windows(3):');
 
 if Choice ==2
         dionysis = '/media/dionysis/';
@@ -12,12 +12,12 @@ elseif Choice == 1;
     
     dropbox = '/Users/Dengyu/Dropbox (Brain Modulation Lab)/';
   
-else %% to be modified
+else % windows
 
     
-    docDir = '\\136.142.16.9\Nexus\Users\pwjones\docs\ET-PD project';
-    codeDir = '\\136.142.16.9\Nexus\Users\pwjones\code\SpeechPilotPD-ET';
-    datadir='\\136.142.16.9\Nexus\Electrophysiology_Data\DBS_Intraop_Recordings';
-    figDir = '\\136.142.16.9\Nexus\Users\dwang\ET-PD\Results\Figures';
-    savedDataDir = '\\136.142.16.9\Nexus\Users\dwang\ET-PD\datafiles';
+    dionysis = '\\136.142.16.9\Nexus\';
+    %codeDir = '\\136.142.16.9\Nexus\Users\pwjones\code\SpeechPilotPD-ET';
+    %datadir='\\136.142.16.9\Nexus\Electrophysiology_Data\DBS_Intraop_Recordings';
+    %figDir = '\\136.142.16.9\Nexus\Users\dwang\ET-PD\Results\Figures';
+    %savedDataDir = '\\136.142.16.9\Nexus\Users\dwang\ET-PD\datafiles';
 end
