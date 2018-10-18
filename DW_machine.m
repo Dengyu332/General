5391c8d1be7156a88e495f283ab431a9d86b7b79
Wang <DEW93@pitt.edu>
2018-10-18 10:57:44 -0400

    modifiy the dropbox dir to directly within Functions/Dengyu/

--- DW_machine.m
+++ DW_machine.m
@@ -4,17 +4,17 @@ Choice=input('Please specify mac(1), linux(2) or Windows(3):');
 
 if Choice ==2
         dionysis = '/media/dionysis/';
-        dropbox = '/home/Dengyu/Dropbox (Brain Modulation Lab)/';
+        dropbox = '/home/Dengyu/Dropbox (Brain Modulation Lab)/Functions/Dengyu/';
 
 
 elseif Choice == 1;
     dionysis = '/Volumes/Nexus/';
     
-    dropbox = '/Users/Dengyu/Dropbox (Brain Modulation Lab)/';
+    dropbox = '/Users/Dengyu/Dropbox (Brain Modulation Lab)/Functions/Dengyu/';
   
 else % windows
 
-    
+    dropbox = 'E:\LeadDBSpractice\Dengyu\git\';
     dionysis = '\\136.142.16.9\Nexus\';
     %codeDir = '\\136.142.16.9\Nexus\Users\pwjones\code\SpeechPilotPD-ET';
     %datadir='\\136.142.16.9\Nexus\Electrophysiology_Data\DBS_Intraop_Recordings';
